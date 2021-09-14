7a2040a889559eddd71a8bc51abbe6f1b5df8ed2
Gary Hutson <g.hutson@nhs.net>
2021-09-14 16:53:43 +0100

    Update 03_ParelleliseHPTUNING.py

--- 03_ParelleliseHPTUNING.py
+++ 03_ParelleliseHPTUNING.py
@@ -1,5 +1,5 @@
 # =============================================================================
-# 02 Parallel Hyperparameter Tuning
+# 03 Parallel Hyperparameter Tuning
 # =============================================================================
 from time import time
 from sklearn.datasets import make_classification
@@ -7,7 +7,7 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 import os
-
+import matplotlib.pyplot as plt
 # Get CPU cores
 cpu_count = os.cpu_count()
 print(f"This machine has {cpu_count} cores")
@@ -25,7 +25,7 @@ X, Y = make_classification(n_samples=1000,
 #------------------------------------------------------------------------------
 # Train model on max number of CPUs
 #------------------------------------------------------------------------------
-model = RandomForestClassifier(n_estimators=100, n_jobs=1)
+rf_model = RandomForestClassifier(n_estimators=100, n_jobs=1)
 
 #------------------------------------------------------------------------------
 # Grid search hyperparameter tuning
@@ -36,16 +36,18 @@ grid = dict()
 grid['max_features'] = [1,2,3,4,5]
 # Define the grid 
 
-def grid_search(model, grid, n_jobs, cv, **args):
+#------------------------------------------------------------------------------
+# Create grid search function
+#------------------------------------------------------------------------------
+def grid_search_cv(model, grid, n_jobs, cv):
     print("[GRID SEARCH INFO] Starting the grid search")
-    grid_search = GridSearchCV(model=model, grid=grid, n_jobs=n_jobs, cv=cv)
+    grid_search = GridSearchCV(model, grid, n_jobs=n_jobs, cv=cv)
     start = time()
-    grid_search = grid_search.fit(X, Y)
+    grid_search_fit = grid_search.fit(X, Y)
     end = time()
-    #Print out search time
     finish_time = end - start
     print("[GRID SEARCH EXECUTION TIME] the search finished in: {:.3f} seconds.".format(finish_time))
-    return [grid_search, finish_time]
+    return [finish_time, grid_search, grid_search_fit]
 
 
 #------------------------------------------------------------------------------
@@ -55,11 +57,21 @@ results_list = list()
 # Create a list with number of cores
 cores = [core for core in range(1, final_cpu+1)]
 # Get the cores list dynamically, as previous example was hard coded
+#result = grid_search_cv(rf_model, grid=grid, n_jobs=2, cv=cv)
 for n_cor in cores:
-    grid_search = grid_search(model=model, 
-                              grid=grid, 
-                              n_jobs=n_cor,
-                              cv=cv)
+    search = grid_search_cv(rf_model, grid=grid, n_jobs=n_cor, cv=cv)
+    result = search[0]
+    results_list.append(result)
+
+# Generate plot of results
+def plot_results(x_val, y_val):
+   plt.plot(x_val, y_val, color="blue", linestyle="--", marker="o")
+   plt.xlabel("Number of cores")
+   plt.ylabel("Run time (secs)")
+   plt.show() 
+   
+plot_results(cores, results_list)
+
     
 
 
